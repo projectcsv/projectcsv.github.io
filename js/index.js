03d66561285e98ddbc16eb1890e48882c2923358
Luke Melaia <lukemelaia.personal@gmail.com>
2019-04-11 01:06:51 +0200

    Added visibility toggle to controls.

--- js/index.js
+++ js/index.js
@@ -14,3 +14,71 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+/**
+ * Basic controls toggle button callback.
+ */
+$("#basic-controls-toggle").click(function() {
+  toggleBasic();
+});
+
+/**
+ * Hides the basic controls.
+ */
+function hideBasic(){
+    $("#basic-controls").hide();
+    $("#basic-controls-display").html("arrow_drop_up");
+}
+
+/**
+ * Shows the basic controls.
+ */
+function showBasic(){
+    $("#basic-controls").show();
+    $("#basic-controls-display").html("arrow_drop_down");
+}
+
+/**
+ * Toggles the basic controls visibility.
+ */
+function toggleBasic(){
+    if ($("#basic-controls").is(":visible")){
+        hideBasic();
+    } else {
+        showBasic();
+    }
+}
+
+/**
+ * Advanced controls toggle button callback.
+ */
+$("#advanced-controls-toggle").click(function() {
+  toggleAdvanced();
+});
+
+/**
+ * Hides the advanced controls.
+ */
+function hideAdvanced(){
+    $("#advanced-controls").hide();
+    $("#advanced-controls-display").html("arrow_drop_up");
+}
+
+/**
+ * Shows the advanced controls.
+ */
+function showAdvanced(){
+    $("#advanced-controls").show();
+    $("#advanced-controls-display").html("arrow_drop_down");
+}
+
+/**
+ * Toggles the advanced controls visibility.
+ */
+function toggleAdvanced(){
+    if ($("#advanced-controls").is(":visible")){
+        hideAdvanced();
+    } else {
+        showAdvanced();
+    }
+}
